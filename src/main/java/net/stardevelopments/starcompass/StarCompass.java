fc5e8fde21f5625f10b4b209b956677ea075780f
starfihgter <alexi.rampono.kelly@gmail.com>
2020-10-06 09:17:24 +1100

    2.2
    UNTESTED - (hopefully) fixed NullPointerException throwing when clicking with an empty hand, gives player compass with player name on command

--- src/main/java/net/stardevelopments/starcompass/StarCompass.java
+++ src/main/java/net/stardevelopments/starcompass/StarCompass.java
@@ -13,9 +13,11 @@ package net.stardevelopments.starcompass;
         import org.bukkit.event.player.PlayerPortalEvent;
         import org.bukkit.inventory.ItemStack;
         import org.bukkit.inventory.meta.CompassMeta;
+        import org.bukkit.inventory.meta.ItemMeta;
         import org.bukkit.plugin.java.JavaPlugin;
         import org.bukkit.plugin.Plugin;
 
+        import javax.annotation.Nonnull;
         import java.util.Objects;
         import java.util.Timer;
         import java.util.TimerTask;
@@ -43,6 +45,13 @@ public final class StarCompass extends JavaPlugin implements Listener {
         Player player = Bukkit.getPlayerExact(target);
         sender.sendMessage(target + " is now being tracked!");
         player.sendMessage(sender.getName() + " is tracking you!");
+
+        ItemStack newCompass = new ItemStack(Material.COMPASS);
+        ItemMeta compassMeta = newCompass.getItemMeta();
+        compassMeta.setDisplayName(target + " tracker");
+        newCompass.setItemMeta(compassMeta);
+        Player pSender = (Player) sender;
+        pSender.getInventory().addItem(newCompass);
         return true;
     }
 
@@ -50,7 +59,11 @@ public final class StarCompass extends JavaPlugin implements Listener {
     public void onPlayerInteract(PlayerInteractEvent event){
         Player player = event.getPlayer();
         ItemStack compass = event.getItem();
-        if (compass != null) {
+        try{
+            compass.getType();
+        }catch(NullPointerException e){
+            return;
+        }
             if (compass.getType() == Material.getMaterial("COMPASS")) {
                 if (Objects.requireNonNull(event.getItem()).getType() == Material.getMaterial("COMPASS")) {
                     if (target == null) {
@@ -70,7 +83,6 @@ public final class StarCompass extends JavaPlugin implements Listener {
                     }
                 }
             }
-        }
     }
 
    // @EventHandler
